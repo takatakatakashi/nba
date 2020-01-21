9343be68ae3b6da78c6c01246345affbe3488797
takatakatakashi <iverson3kobe0824@gmil.com>
2020-01-21 16:26:39 +0900

    add

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,29 @@
+class TweetsController < ApplicationController
+
+  def index
+    @tweets = Tweet.all
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  def show
+    Tweet.show(tweet_params)
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:image, :text, :title)
+  end
+
+end
